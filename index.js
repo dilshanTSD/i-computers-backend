fdbc907026d2da82e092e8b6fdc6c95aeb682f33
dilshanTSD <info.tsddigital@gmail.com>
2026-05-24 16:26:06 +0530

    initialization 🚀

---
+++ index.js
@@ -0,0 +1,33 @@
+import express, { application } from 'express'
+import mongoose from 'mongoose'
+import studentRouter from './routes/studentRouter.js'
+import userRouter from './routes/userRouter.js'
+import authenticateUser from './middlewares/authenticate.js'
+import productRouter from './routes/productRouter.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const mongoUri = process.env.MONGO_URI
+
+mongoose.connect(mongoUri).then(
+    ()=>{
+        console.log("Connected to MongoDB")
+    }
+)
+
+
+const app = express()
+
+app.use( express.json() )
+
+app.use(authenticateUser)
+
+app.use("/students", studentRouter)
+app.use("/users", userRouter)
+app.use("/products", productRouter)
+ 
+app.listen( 3000 ,
+    ()=>{
+      console.log("Server is running!")  
+    }
+)
